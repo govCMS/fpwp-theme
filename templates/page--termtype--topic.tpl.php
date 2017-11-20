644796eca2913a21563194c8cfeac0ce6052a765
Tim Siers <tim@oxideinteractive.com.au>
2017-11-20 18:22:02 +1100

    Remove colon after title on topic pages

--- templates/page--termtype--topic.tpl.php
+++ templates/page--termtype--topic.tpl.php
@@ -41,7 +41,7 @@
           <a id="main-content"></a>
           <?php print render($title_prefix); ?>
           <?php if ($title): ?>
-            <h1 class="page__title title" id="page-title"><?php print $title; ?>:&nbsp;</h1>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?>&nbsp;</h1>
           <?php endif; ?>
           <?php print render($title_suffix); ?>
           <?php print render($taxonomy_term->content['field_subtitle']); ?>
